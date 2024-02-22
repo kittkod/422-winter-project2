237764dd927d7534ac081265a9311c7cad0adc6f
Jasmine Wallin <jwallin2@uoregon.edu>
2024-02-21 20:21:31 -0800

    Update Resource_Graph.py

--- Resource_Graph.py
+++ Resource_Graph.py
@@ -1,6 +1,5 @@
 """
 Grapher to display free food resources around UO (as well as eugene, if development goes there)
-Jasmine Wallin
 """
 # requires two imports - plotly and pandas. plotly requires pandas to work. 
 
@@ -10,7 +9,7 @@ import plotly.express as px
 us_cities = pd.read_csv("./testlocations.csv")
 
 fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", title="Map of Food Resources", 
-    height=650, width=1200, zoom=14.6, text=us_cities.comment, hover_name=us_cities.comment,
+    height=650, width=1200, zoom=14.6, text="comment", hover_name="comment",
     hover_data={"text":True, "Food Resources":False, "sizes":False, "comment":False, "lat":False, "lon":False}, 
     size=us_cities.sizes, color="Food Resources", color_continuous_scale="red", labels={'text':''})
 
