19ad38de5d98ba4cc59f35e9acf5964e8668efbb
Nithi <dihtin@gmail.com>
2024-02-29 14:10:33 -0800

    pop-up buttons working
    i'll add the links to the extra resources tn

--- Food_Information_User_Interface.py
+++ Food_Information_User_Interface.py
@@ -78,12 +78,15 @@ scrollable_frame_food_list = ctk.CTkScrollableFrame(
     )
 scrollable_frame_food_list.pack()
 
+#######################################################################
+
 def show_resource_list():
     resource_list_window = ctk.CTkToplevel()
     resource_list_window.title('Extra Free Food Resources')
 
     # Use standard Tkinter Listbox instead of CTkListbox
     resource_listbox = tk.Listbox(resource_list_window)
+
 
     # Insert some items into the listbox for testing
     resource_listbox.insert(1, "Resource 1")
@@ -98,8 +101,6 @@ def show_resource_list():
 # Create a button to show the free food resources list
 resource_button = ctk.CTkButton(window, text='Extra Resources', command=show_resource_list)
 resource_button.pack()
-
-
 
 #######################################################################
 
