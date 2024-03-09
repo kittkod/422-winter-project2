f337f83793f2047125699ec717487483f565b848
kyliecharlotte <kcg3@icloud.com>
2024-03-09 14:41:26 -0800

    pushed file to run dollarless dining

---
+++ Dollarless_Dining.py
@@ -0,0 +1,12 @@
+"""
+Runs Dollarless_Dining.py program
+"""
+import coordinate_finder
+import Remodeled_Food_Information_User_Interface
+
+def main():
+    coordinate_finder.main()
+    Remodeled_Food_Information_User_Interface
+
+if __name__ == "__main__":
+    main()
