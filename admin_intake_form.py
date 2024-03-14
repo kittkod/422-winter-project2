44c51699ce290e1a1c295cd658b10570d7dcc92d
kyliecharlotte <kcg3@icloud.com>
2024-03-13 19:26:18 -0700

    documentation changes!

--- admin_intake_form.py
+++ admin_intake_form.py
@@ -26,6 +26,10 @@ unprocessed_admin_CSV_file = "dollarless_database_files/unprocessed_admin_info.c
 admin_CSV_file = "dollarless_database_files/admin_info.csv"
 
 def add_to_admin_file(dictionary):
+    """Function to add manual input into admin_info.csv
+    input: dict
+    output: void
+    """
 
     with open('campus_buildings.txt') as f: 
         data = f.read() 
@@ -36,6 +40,7 @@ def add_to_admin_file(dictionary):
     class_dictionary = js_file
 
     admin_CSV_entry = []
+    #Pull location from the dictionary received from the Food_Information_User_Interface
     admin_location = dictionary.get("location")
     admin_CSV_entry.append(dictionary.get("title"))
     admin_CSV_entry.append(dictionary.get("date"))
@@ -44,70 +49,94 @@ def add_to_admin_file(dictionary):
     admin_CSV_entry.append(admin_location)
     admin_CSV_entry.append(dictionary.get("desc"))
     admin_CSV_entry.append(dictionary.get("organizers"))
+
+    #Code to check if a location is a building on campus or an address in Eugene
     matched_location = None
     if admin_location:
         for location in class_dictionary.keys():
+            #check keys of dictionary for a match
             if location in admin_location:
                 matched_location = location
                 break
         if matched_location == None:
+            #if match is not found, use raw address
             new_location = address_converter(admin_location)
             lat, long = lat_and_long(new_location)
         else:
+            #if a match is found, pull information from dictionary
             latlong = class_dictionary.get(matched_location).split(" ")
             lat = latlong[0]
             long = latlong[1]
+
     admin_CSV_entry.append(lat)
     admin_CSV_entry.append(long)
-    admin_CSV_entry.append(False)
+    admin_CSV_entry.append(False) #mark reoccuring event as FALSE
 
     if os.path.isfile("./dollarless_database_files/unprocessed_admin_info.csv") is False:
-        CSV_file_creator(unprocessed_admin_CSV_file)
+        #check if file exists
+        CSV_file_creator(unprocessed_admin_CSV_file) #add to unprocessed_admin and admin_CSV
         CSV_data_inputter(admin_CSV_entry, unprocessed_admin_CSV_file)
     else:
         CSV_data_inputter(admin_CSV_entry, unprocessed_admin_CSV_file)
     return
 
 def contains_year(input_string):
-    pattern = r'\b\d{4}\b'  # Regular expression pattern for a four-digit number
+    """Function to check if a string contains a year
+    input: str
+    output: bool
+    """
+    pattern = r'\b\d{4}\b'  #regular expression pattern for a four-digit number
     match = re.search(pattern, input_string)
     return match is not None
 
 def convert_time_format(time_str):
+    """Function to convert a time string into a datetime object
+    input: str
+    output: datetime object
+    """
     try:
-        # Parse the input time string
+        #parse the input time string
         time_obj = datetime.strptime(time_str, '%I:%M%p')
         # Format the time object with AM/PM
         formatted_time = time_obj.strftime('%I:%M %p')
     except ValueError:
-        # If parsing fails, return the original string
+        #if parsing fails, return the original string
         formatted_time = time_str
     return formatted_time
 
 def admin_file_updater():
-    """Checks if admin file contains out-of-date data
-    - should only be called in conjunction with updating data"""
+    """Function to update admin_file in congruence with "refresh data" - updates CSV if event has passed
+    input: void
+    output: void
+    """
 
-    ###checks admin file inputs to see if they have passed###
+    #checks admin file inputs to see if they have passed
     original_admin_df = pd.read_csv("./dollarless_database_files/admin_info.csv")
+
     if original_admin_df.empty == True:
+        #if no admin input, exit
         return
+    
     admin_df = original_admin_df[["Date", "Start Time", "End Time"]]
     admin_df = pd.DataFrame(admin_df)
 
-    index = 0
+    index = 0   
     values = []
     time = datetime.now()
-    time_hour = int(datetime.now().time().strftime('%H%M'))
-    for date in admin_df['Date']:
+    time_hour = int(datetime.now().time().strftime('%H%M')) #check the current time
 
+    for date in admin_df['Date']:
+        #check the date on each event
         if contains_year(date) == False:
-            """if year is not in the time string"""
+            # if the year is not in the string, add the year
             fixed_date = datetime.strptime(date, '%B %d')
+
             if fixed_date.date() < time.replace(year=fixed_date.year).date:
                 original_admin_df = original_admin_df.drop(index)
+
             if fixed_date.date() == time.replace(year=fixed_date.year).date:
                 target_time = int(datetime.strptime(admin_df.at[index, "Start Time"], '%I:%M %p').time().strftime('%H%M'))
+
                 if time_hour > target_time:
                     original_admin_df = original_admin_df.drop(index)
                 else:
@@ -116,14 +145,16 @@ def admin_file_updater():
                 values.append(index)
 
         else:
-            """if year is in the time string"""
+            # if year is in the string
             given_datetime = datetime.strptime(date, "%B %d %Y")
-            # Compare the parsed datetime with the current datetime
+            # compare the parsed datetime with the current datetime
             if given_datetime.date() < time.date():
                 original_admin_df = original_admin_df.drop(index)
+
             if given_datetime.date() == time.date():
                 editted_time = convert_time_format(admin_df.at[index, "Start Time"])
                 target_time = int(datetime.strptime(editted_time, '%I:%M %p').time().strftime('%H%M'))
+
                 if time_hour > target_time:
                     original_admin_df = original_admin_df.drop(index)
                 else:
@@ -133,15 +164,15 @@ def admin_file_updater():
 
         index += 1
     
-    CSV_file_creator(admin_CSV_file)
+    CSV_file_creator(admin_CSV_file) #re-create the admin file to only input valid events 
 
     for val in values:
 
         CSV_file_list = original_admin_df.loc[val, :].values.flatten().tolist()
-        CSV_data_inputter(CSV_file_list, admin_CSV_file)
+        CSV_data_inputter(CSV_file_list, admin_CSV_file) #add upcoming events into the admin_CSV and food_CSV 
         CSV_data_inputter(CSV_file_list, food_CSV_file)
 
-    ###unprocessed admin file checker###
+    #unprocessed admin file checker - same process as above but for the unprocessed_admin file
     original_admin_df = pd.read_csv("./dollarless_database_files/unprocessed_admin_info.csv")
     admin_df = original_admin_df[["Date", "Start Time", "End Time"]]
     admin_df = pd.DataFrame(admin_df)
@@ -191,29 +222,36 @@ def admin_file_updater():
         CSV_file_list = original_admin_df.loc[val, :].values.flatten().tolist()
         CSV_data_inputter(CSV_file_list, "unprocessed_admin_info.csv")
 
-
 def delete_from_admin(event_title: str):
-    """Function to remove admin entered data - once admin has selected an event title to delete"""
-    ##should be no errors in event_title because event_title is a button press grab
-    
+    """Function to delete manual admin data
+    input: str
+    output: void
+    """
+    #Read both CSV files
     df_admin_1 = pd.read_csv(admin_CSV_file)
     df_admin = pd.DataFrame(df_admin_1)
+
     df_food_1 = pd.read_csv(food_CSV_file)
     df_food = pd.DataFrame(df_food_1)
+    
+    #get the index of the event to be deleted
     admin_index = df_admin.index.get_loc(df_admin[df_admin['Event Title'] == event_title].index[0])
     food_index = df_food.index.get_loc(df_food[df_food['Event Title'] == event_title].index[0])
-    df_admin = df_admin.drop([admin_index])
-    df_food = df_food.drop([food_index])
+    df_admin = df_admin.drop([admin_index]) #remove from admin_info.csv
+    df_food = df_food.drop([food_index]) #remove from free_food_database.csv
 
     CSV_file_creator(admin_CSV_file)
-    df_admin.to_csv('./dollarless_database_files/admin_info.csv', index=False)
+    df_admin.to_csv('./dollarless_database_files/admin_info.csv', index=False) #remake dataframe into CSV file 
+
     CSV_file_creator(food_CSV_file)
-    df_food.to_csv('./dollarless_database_files/Free_Food_Database.csv', index=False)
+    df_food.to_csv('./dollarless_database_files/Free_Food_Database.csv', index=False) #remake dataframe into CSV file 
     return  
 
 def delete_only_from_admin(event_title: str):
-    """Function to remove admin entered data (but only on admin list)"""
-    ##should be no errors in event_title because event_title is a button press grab
+    """Function to delete manual admin data but only from admin event - test function
+    input: str
+    output: void
+    """
     
     df_admin_1 = pd.read_csv(admin_CSV_file)
     df_admin = pd.DataFrame(df_admin_1)
